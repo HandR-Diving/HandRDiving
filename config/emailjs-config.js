93420babb01426f73a56634fdd9896900a9ebaf7
aschlot2 <Al.Schlotterer+GitHub@gmail.com>
2026-07-09 23:13:37 -0400

    dev: inital page layout with images and form.

---
+++ config/emailjs-config.js
@@ -0,0 +1,12 @@
+window.EMAILJS_CONFIG = {
+  publicKey: 'TODO',
+  serviceId: 'TODO',
+  templateId: 'TODO',
+  options: {
+    blockHeadless: true,
+    limitRate: {
+      id: 'hrd-contact-form',
+      throttle: 10000,
+    },
+  },
+};
